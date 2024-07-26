de20c065af17c5bd9f21a5bffeed5da3646a483c
Darshan <darshanbpatel1904@gmail.com>
2024-07-26 15:09:47 +0530

    Add SpeedInsights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode> 
     <BrowserRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>,
   {/* </React.StrictMode>, */}
 )
